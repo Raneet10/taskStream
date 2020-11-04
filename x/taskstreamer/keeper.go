df7909ae26538eeef1297264ecf8f5f0aad4598e
raneet <raneetdebnath10@gmail.com>
2020-11-04 11:19:55 +0530

    changed directory structure and added few getters in keeper

--- x/taskstreamer/keeper.go
+++ x/taskstreamer/keeper.go
@@ -5,6 +5,7 @@ import (
 	"github.com/cosmos/cosmos-sdk/x/bank"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/raneet10/taskStream/x/taskstreamer/types"
 )
 
 type Keeper struct {
@@ -24,7 +25,36 @@ func NewKeeper(bank bank.Keeper, taskStore sdk.StoreKey, cdc *codec.Codec) Keepe
 }
 
 // Get a individual task
+func (k Keeper) GetTask(ctx sdk.Context, key string) (types.Task, error) {
+	store := sdk.KVStrore(k.taskStore)
+	var task types.Task
+	byteKey := []byte(types.TaskPrefix + key)
+	err := k.cdc.UnmarshalBinaryLengthPrefixed(store.Get(byteKey), &task)
+
+	if err != nil {
+		return task, err
+	}
+	return task, nil
+}
+
 // Get all tasks
+func (k Keeper) GetAllTasks(ctx sdk.Context) ([]types.Task, error) {
+	store := sdk.KVStore(k.taskStore)
+	var tasks []types.Task
+	iterator := sdk.KVStorePrefixIterator(store, []byte(types.TaskPrefix))
+
+	for ; iterator.Valid(); iterator.Next() {
+		var task types.Task
+		err := k.cdc.MustUnmarshalBinaryLengthPrefixed(store.Get(iterator.Key()), &task)
+		if err != nil {
+			return tasks, err
+		}
+		tasks = append(tasks, task)
+	}
+
+	return tasks, nil
+}
+
 // CreateTask
 // Add value to task - add new value giver as backer
 // Give proof of compeltion
---
+++ x/taskstreamer/types/key.go
@@ -0,0 +1,15 @@
+package types
+
+const (
+	ModuleName = "taskstreamer"
+
+	StoreKey = ModuleName
+
+	RouterKey = ModuleName
+
+	QuerierRoute = ModuleName
+)
+
+const (
+	TaskPrefix = "task-"
+)
